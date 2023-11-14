550b59ad23aac552a75f46dcd973bf55596f04ba
SanchezMaximo <maximosanchez420@gmail.com>
2023-11-13 23:38:00 -0300

    changesInControllerAndServices

--- CafeteriaWebApi/Services/Implementations/OrderService.cs
+++ CafeteriaWebApi/Services/Implementations/OrderService.cs
@@ -1,12 +1,14 @@
 ﻿using CafeteriaWebApi.Data;
 using CafeteriaWebApi.Data.Entities;
 using CafeteriaWebApi.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CafeteriaWebApi.Services.Implementations
 {
     public class OrderService : IOrderService
     {
         private readonly CafeteriaContext _context;
+
 
         public OrderService(CafeteriaContext context)
         {
@@ -27,5 +29,13 @@ namespace CafeteriaWebApi.Services.Implementations
         {
             throw new NotImplementedException();
         }
+
+        public Order? GetOrder(int orderId)
+        {
+            return _context.Orders
+                
+                .SingleOrDefault(o => o.IdOrder == orderId);
+        }
+
     }
 }
