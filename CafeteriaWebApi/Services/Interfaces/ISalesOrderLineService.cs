864eccbe6e8f4722ea9faf2ec4ef9bffa7be0761
Lorenzo <lorenzocarignani@outlook.com>
2023-11-23 18:47:52 -0300

    addSaleOrderLine&&EnumStateUser

--- CafeteriaWebApi/Services/Interfaces/ISalesOrderLineService.cs
+++ CafeteriaWebApi/Services/Interfaces/ISalesOrderLineService.cs
@@ -1,6 +1,12 @@
-﻿namespace CafeteriaWebApi.Services.Interfaces
+﻿using CafeteriaWebApi.Data.Entities;
+
+namespace CafeteriaWebApi.Services.Interfaces
 {
     public interface ISalesOrderLineService
     {
+        public int CreateSaleOrderLine(SaleOrderLine sol);
+        public int UpdateSaleOrderLine(SaleOrderLine sol);
+        public void DeleteSaleOrderLine(int idSol);
+        public SaleOrderLine GetSaleOrderLineById(int idSol);
     }
 }
